0be27d613ff110d52a805b965de476d7f1f2e481
JeSappelJean <40351638+JeSappelJean@users.noreply.github.com>
2020-05-15 15:23:01 +0200

    TDD 2 : Test créé / Class implémenté

--- Tests/src/Adherent.php
+++ Tests/src/Adherent.php
@@ -17,8 +17,8 @@ class Adherent {
     // Aide : https://murviel-info-beziers.com/fonction-php-supprimer-accents/
     public static function skip_accents(string $string) {
         $charset='utf-8';
-        $str = htmlentities( $string, ENT_NOQUOTES, $charset );
 
+        $str = htmlentities( $string, ENT_NOQUOTES, $charset );
         $str = preg_replace( '#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str );
         $str = preg_replace( '#&([A-za-z]{2})(?:lig);#', '\1', $str );
         $str = preg_replace( '#&[^;]+;#', '', $str );
